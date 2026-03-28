80f9362dfb89c815a02e214f72d015ff958101f4
Salman Ahmad <s.ahmad0147@gmail.com>
2026-03-28 14:40:21 -0600

    chore: reorder home section flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
     <>
       <Hero />
       <QuickInfo />
-      <ServersList />
-      <QuickStartGuide />
       <Features />
+      <QuickStartGuide />
+      <ServersList />
       <JoinCTA />
     </>
   );
